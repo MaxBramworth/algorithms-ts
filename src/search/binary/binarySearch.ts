d06c77b7b96c9adb94ff44db3ed8be572a44c7a2
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-24 10:48:13 +0000

    not yet searching bianrily

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,7 +1,18 @@
 import { NO_MATCH } from "../common"
 
 const binarySearch = (items: any[], item: any): number => {
-
+    let index : number;
+    let lastitem : number = items.length;
+    let firstitem : number = 0;
+    let itemrange : number = 0;
+    while (true){
+        index = (itemrange - (itemrange % 2)) / 2 + (itemrange % 2);
+        if (items[index] === item){
+            return(index);
+        } else if (items[index] > item){
+            items
+        }
+    }
     return NO_MATCH;
 }
 
