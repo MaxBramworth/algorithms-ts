b5010a8bb01e9cf9bb13a5d0c3a2dfc849eaa655
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-25 09:45:51 +0000

    disfunctional binary search that doesnt work

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,19 +1,29 @@
 import { NO_MATCH } from "../common"
 
 const binarySearch = (items: any[], item: any): number => {
-    let index : number;
-    let lastitem : number = items.length;
-    let firstitem : number = 0;
-    let itemrange : number = 0;
-    while (true){
-        index = (itemrange - (itemrange % 2)) / 2 + (itemrange % 2);
-        if (items[index] === item){
-            return(index);
-        } else if (items[index] > item){
-            items
+    let index : number; // index we looking at
+    let lastitem : number = items.length; // last item in our mini list
+    let firstitem : number = 0; // first item in our mini list
+    let done : any;
+    while (done === null){
+        index = Math.floor((firstitem + lastitem) / 2); // sets index to the midpoint of the mini list
+        if (items[index] === item){ // if this is the thing
+            done = index;
+        } else if (firstitem === lastitem){
+            done = NO_MATCH;
+        } else if (items[index] > item){ // if it is bigger than item
+            if (index === 0){
+                done = NO_MATCH;
+            }
+            lastitem = index - 1;
+        } else if (items[index] < item){ // if it is smoller than item
+            if (index === items.length - 1){
+                done = NO_MATCH;
+            }
+            firstitem = index + 1;
         }
     }
-    return NO_MATCH;
+    return done; // return
 }
 
 export default binarySearch;
